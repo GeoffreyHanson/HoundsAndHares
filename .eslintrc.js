5e50989d7083bc890656b67097a53a8c3db637f3
GeoffreyHanson <g.hanson.b@gmail.com>
2018-10-18 20:26:01 -0500

    Added all setup files.

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "plugin:prettier/recommended"
+  ],
+  env: {
+    node: true,
+    browser: true,
+    jquery: true,
+    mocha: true,
+    commonjs: true,
+    es6: true
+  }
+};
