92d939af51615636561152136f9039a8b675d6fa
Michael Cumings <michael.cumings@gmail.com>
2018-10-20 12:41:21 -0500

    firebase signin

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -48,6 +48,33 @@ initApp = function() {
         document.getElementById("sign-in-status").textContent = "Signed out";
         document.getElementById("sign-in").textContent = "Sign in";
         document.getElementById("account-details").textContent = "null";
+        // FirebaseUI config.
+        const uiConfig = {
+          signInSuccessUrl: "<url-to-redirect-to-on-success>",
+          signInOptions: [
+            // Leave the lines as is for the providers you want to offer your users.
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+            firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+            firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+          ],
+          // tosUrl and privacyPolicyUrl accept either url string or a callback
+          // function.
+          // Terms of service url/callback.
+          tosUrl: "<your-tos-url>",
+          // Privacy policy url/callback.
+          privacyPolicyUrl() {
+          window.location.assign('<your-privacy-policy-url>');
+        }
+        };
+
+        // Initialize the FirebaseUI Widget using Firebase.
+        let ui = new firebaseui.auth.AuthUI(firebase.auth());
+        // The start method will wait until the DOM is loaded.
+        ui.start("#firebaseui-auth-container", uiConfig);
       }
     },
     error => {
