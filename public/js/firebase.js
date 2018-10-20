9c5bb16ebde3b176ade07a53b0f12b2802f0e8e0
Michael Cumings <michael.cumings@gmail.com>
2018-10-20 12:49:26 -0500

    fb try 3

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -53,12 +53,12 @@ initApp = function() {
           signInSuccessUrl: "<url-to-redirect-to-on-success>",
           signInOptions: [
             // Leave the lines as is for the providers you want to offer your users.
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-            firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+            // firebase.auth.GithubAuthProvider.PROVIDER_ID,
             firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+            // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
             firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
           ],
           // tosUrl and privacyPolicyUrl accept either url string or a callback
@@ -67,12 +67,12 @@ initApp = function() {
           tosUrl: "<your-tos-url>",
           // Privacy policy url/callback.
           privacyPolicyUrl() {
-          window.location.assign('<your-privacy-policy-url>');
-        }
+            window.location.assign("<your-privacy-policy-url>");
+          }
         };
 
         // Initialize the FirebaseUI Widget using Firebase.
-        let ui = new firebaseui.auth.AuthUI(firebase.auth());
+        const ui = new firebaseui.auth.AuthUI(firebase.auth());
         // The start method will wait until the DOM is loaded.
         ui.start("#firebaseui-auth-container", uiConfig);
       }
