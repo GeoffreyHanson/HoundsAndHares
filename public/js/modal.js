f47fc346af902630a3c4ea0a7a3c0ee5c23d6009
Michael Cumings <michael.cumings@gmail.com>
2018-10-24 19:34:29 -0500

    modal4

---
+++ public/js/modal.js
@@ -0,0 +1,14 @@
+const modal = document.querySelector("#modal");
+const modalOverlay = document.querySelector("#modal-overlay");
+const closeButton = document.querySelector("#close-button");
+const openButton = document.querySelector("#open-button");
+
+closeButton.addEventListener("click", () => {
+  modal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+});
+
+openButton.addEventListener("click", () => {
+  modal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+});
