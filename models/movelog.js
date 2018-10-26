94403646910329514dd06453ff74eb5b657a29ce
Geoffrey Hanson <g.hanson.b@gmail.com>
2018-10-26 12:55:54 -0500

    Made models for moves, users, and games.

--- models/movelog.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Move = sequelize.define("Move", {
-    piece: DataTypes.STRING,
-    code: DataTypes.STRING,
-    player: DataTypes.INTEGER
-  });
-  return Move;
-};
