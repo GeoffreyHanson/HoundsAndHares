7b1906316ccb64c013021341d90d77474073220b
Geoffrey Hanson <g.hanson.b@gmail.com>
2018-11-04 19:49:37 -0600

    Fixed seeding issue

--- models/piece.js
+++ models/piece.js
@@ -9,21 +9,25 @@ module.exports = (sequelize, DataTypes) => {
   // Should probably do this on the front end.
   Piece.sync().then(() => {
     Piece.create({
+      id: 1,
       pieceName: "houndOne",
       cx: "250",
       cy: "50"
     });
     Piece.create({
+      id: 2,
       pieceName: "houndTwo",
       cx: "100",
       cy: "200"
     });
     Piece.create({
+      id: 3,
       pieceName: "houndThree",
       cx: "250",
       cy: "350"
     });
     Piece.create({
+      id: 4,
       pieceName: "hare",
       cx: "400",
       cy: "200"
